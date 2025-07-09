cab746244589d3883da07946e1f19fd1168be9df
GitUser <120816634+Ursine09@users.noreply.github.com>
2025-07-09 00:46:55 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,10 +1,61 @@
 package gameofWar;
 
+import java.util.LinkedList;
+
 public class Player {
 	
-	int score;
-	String name;
+	private String namePlayer;
+	private int score;
+	private LinkedList<Card> hand;
 	
 	
+	
+	
+	public Player(String name) {
+		// TODO Auto-generated method stub
+		this.namePlayer = name;
+		this.hand = new LinkedList<>();
+		this.score = 0;
+	}
 
-}
+
+
+
+	public String getNamePlayer() {
+		return namePlayer;
+	}
+
+
+
+
+	public void setNamePlayer(String namePlayer) {
+		this.namePlayer = namePlayer;
+	}
+
+
+
+
+	public int getScore() {
+		return score;
+	}
+
+
+
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+
+
+
+	public LinkedList<Card> getHand() {
+		return hand;
+	}
+
+
+
+
+	public void setHand(LinkedList<Card> hand) {
+		this.hand = hand;
+	}
