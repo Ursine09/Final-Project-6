2e78a2a39aba9f0102a57587f70be34e49357fff
GitUser <120816634+Ursine09@users.noreply.github.com>
2025-07-21 17:52:37 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,61 +1,88 @@
 package gameofWar;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
-	
+
 	private String namePlayer;
-	private int score;
-	private LinkedList<Card> hand;
-	
-	
-	
-	
+	int score1;
+	int score2;
+	private List<Card> hand = new ArrayList<>();
+
+	public void describe() {
+		for (Card card : hand) {
+			card.describe();
+		}
+	}
+
+	public Card flip() {
+		return hand.remove(0);
+	}
+
+	public void draw(Deck deck) {
+		hand.add(deck.draw());
+	}
+
+	public void incrementScoreOne() {
+		score1 += 1;
+
+	}
+
+	public void incrementScoreTwo() {
+		score2 += 1;
+
+	}
+
+	public void finaloneScore() {
+		System.out.println("Player One:" + this.score1);
+		System.out.println(" ");
+	}
+
+	public void finaltwoScore() {
+		System.out.println("Player Two:" + this.score2);
+		System.out.println(" ");
+	}
+
+	public void gameFinalScore() {
+		System.out.println("Player One:" + this.score1 + "Player Two" + this.score2);
+		System.out.println(" ");
+
+	}
+
 	public Player(String name) {
 		// TODO Auto-generated method stub
 		this.namePlayer = name;
-		this.hand = new LinkedList<>();
-		this.score = 0;
+		this.score1 = 0;
+		this.score2 = 0;
 	}
 
-
-
+	public Player() {
+		// TODO Auto-generated constructor stub
+	}
 
 	public String getNamePlayer() {
 		return namePlayer;
 	}
 
-
-
-
 	public void setNamePlayer(String namePlayer) {
 		this.namePlayer = namePlayer;
 	}
 
-
-
-
-	public int getScore() {
-		return score;
+	public int getoneScore() {
+		return score1;
 	}
 
-
-
-
-	public void setScore(int score) {
-		this.score = score;
+	public void setooneScore(int score1) {
+		this.score1 = score1;
 	}
 
-
-
-
-	public LinkedList<Card> getHand() {
-		return hand;
+	public int gettwoScore() {
+		return score2;
 	}
 
-
-
-
-	public void setHand(LinkedList<Card> hand) {
-		this.hand = hand;
+	public void settwoScore(int score2) {
+		this.score2 = score2;
 	}
+}
+
